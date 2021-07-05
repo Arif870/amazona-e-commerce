44543f7f674081fc1822d910142334870ab50980
Arif <arifuzzaman959@gmail.com>
2021-07-05 22:30:28 +0600

    product product and ration added by react

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Product from "./components/Product";
 import data from "./data";
 function App() {
   return (
@@ -15,36 +16,7 @@ function App() {
       <main>
         <div className="row center">
           {data.products.map((product) => (
-            <div key={product._id} className="card">
-              <a href={`/product/${product._id}`}>
-                <img className="medium" src={product.image} alt="product" />
-              </a>
-              <a href={`/product/${product._id}`}>
-                <div className="card-body">
-                  <h2>{product.name}</h2>
-                  <div className="rating">
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                    <span>
-                      <i className="fa fa-star"></i>
-                    </span>
-                  </div>
-                  <div className="price">
-                    <p>${product.price}</p>
-                  </div>
-                </div>
-              </a>
-            </div>
+            <Product key={product._id} product={product}></Product>
           ))}
         </div>
       </main>
